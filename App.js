a7a7aeb6d043b5fb4750978db9a259f41041c7c8
Yuri La Torre Ortiz <yuri.la.torre.ortiz@protonmail.ch>
2021-01-08 15:54:19 +0100

    build: import firebase

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
+import * as firebase from 'firebase';
+
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
