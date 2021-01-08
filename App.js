83aa2cc0fb8d77e3509f5a96a33922f251d45f97
Yuri La Torre Ortiz <yuri.la.torre.ortiz@protonmail.ch>
2021-01-08 16:02:58 +0100

    refactor: import Firebase from config.js file

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
-import * as firebase from 'firebase';
+import {firebase} from './src/firebase/config';
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
