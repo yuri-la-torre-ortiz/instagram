201afcdd1b6f6e308f51f9f48eb34e15a0004eec
Yuri La Torre Ortiz <yuri.la.torre.ortiz@protonmail.ch>
2021-01-15 15:55:36 +0100

    feat: create Login screen connected to Firebase

---
+++ components/auth/Login.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import { View, Button, TextInput } from "react-native";
+
+import firebase from "firebase";
+
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: "",
+    };
+
+    this.onLogin = this.onLogin.bind(this);
+  }
+
+  onLogin() {
+    const { email, password } = this.state;
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  render() {
+    return (
+      <View>
+        <TextInput
+          placeholder="email"
+          onChangeText={(email) => this.setState({ email })}
+        />
+        <TextInput
+          placeholder="password"
+          secureTextEntry={true}
+          onChangeText={(password) => this.setState({ password })}
+        />
+        <Button title="Login" onPress={() => this.onLogin()} />
+      </View>
+    );
+  }
+}
