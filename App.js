6804756bb0172e9bcc074ea8a51279879c85f56e
Yuri La Torre Ortiz <yuri.la.torre.ortiz@protonmail.ch>
2021-01-18 14:13:01 +0100

    feat: set state for Firebase authentication

--- App.js
+++ App.js
@@ -13,6 +13,27 @@ import RegistrationScreen from "./components/auth/Register";
 const Stack = createStackNavigator();
 
 export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loaded: false,
+    };
+  }
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if(!user){
+        this.setState({
+          loggedIn: false,
+          loaded: true,
+        })
+      } else {
+        this.setState({
+          loggedIn: true,
+          loaded: true,
+        })
+      }
+    });
+  }
   render() {
     return (
       <NavigationContainer>
