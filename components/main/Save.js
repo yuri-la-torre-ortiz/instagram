560258b4a23cf042ada3d7a8d5e2af179d1cc302
Yuri La Torre Ortiz <yuri.la.torre.ortiz@protonmail.ch>
2021-01-27 13:26:06 +0100

    feat: save image to Firebase Storage

--- components/main/Save.js
+++ components/main/Save.js
@@ -1,10 +1,60 @@
-import React from "react";
-import { View, TextInput, Image } from "react-native";
+import React, { useState } from "react";
+import { View, StyleSheet, TextInput, Image, Button } from "react-native";
+
+import firebase from "firebase";
+require("firebase/firestore");
+require("firebase/firebase-storage");
 
 export default function Save(props) {
-  console.log(props.route.params);
+  const [caption, setCaption] = useState("");
+  console.log(caption);
+
+  const uploadImage = async () => {
+    const uri = props.route.params.image;
+    const childPath = `post/${
+      firebase.auth().currentUser.uid
+    }/${Math.random().toString(36)}`;
+    console.log(childPath);
+
+    const response = await fetch(uri);
+    const blob = await response.blob();
+
+    const task = firebase.storage().ref().child(childPath).put(blob);
+
+    const taskProgress = (snapshot) => {
+      console.log(`transferred: ${snapshot.bytesTransferred}`);
+    };
+
+    const taskCompleted = () => {
+      task.snapshot.ref.getDownloadURL().then((downloadURL) => {
+        console.log("File available at", downloadURL);
+      });
+    };
+
+    const taskError = (snapshot) => {
+      console.log(snapshot);
+    };
+
+    task.on("state_changed", taskProgress, taskError, taskCompleted);
+  };
+
   return (
-    <View>
+    <View style={styles.container}>
+      <Image source={{ uri: props.route.params.image }} style={styles.image} />
+      <TextInput
+        placeholder="Write a caption ..."
+        onChangeText={(caption) => setCaption(caption)}
+      />
+      <Button title="Save" onPress={() => uploadImage()} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+  },
+});
