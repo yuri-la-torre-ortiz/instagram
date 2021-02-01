f1a85d05f17318a2dc3f5ee0b6d9f892475000d2
Yuri La Torre Ortiz <yuri.la.torre.ortiz@protonmail.ch>
2021-02-01 13:31:11 +0100

    feat: get user data with redux for Profile Screen

--- components/main/Profile.js
+++ components/main/Profile.js
@@ -1,10 +1,22 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image, FlatList, StyleSheet } from "react-native";
 
-export default function Profile() {
+import { connect } from "react-redux";
+
+function Profile(props) {
+  const { currentUser, posts } = props;
+  console.log({currentUser, posts});
+
   return (
     <View>
       <Text>Profile Screen</Text>
     </View>
   );
 }
+
+const mapStateToProps = (store) => ({
+  currentUser: store.userState.currentUser,
+  posts: store.userState.posts,
+});
+
+export default connect(mapStateToProps, null)(Profile);
