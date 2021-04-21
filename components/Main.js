bbe80468b4c84c2bcfd1b99470d55b9bec873d18
Yuri La Torre Ortiz <yuri.la.torre.ortiz@protonmail.ch>
2021-04-21 21:19:33 +0200

    feat: pass user id from search through props

--- components/Main.js
+++ components/Main.js
@@ -54,6 +54,14 @@ export class Main extends Component {
         <Tab.Screen
           name="Profile"
           component={ProfileScreen}
+          listeners={({ navigation }) => ({
+            tabPress: (event) => {
+              event.preventDefault();
+              navigation.navigate("Profile", {
+                uid: firebase.auth().currentUser.uid,
+              });
+            },
+          })}
           options={{
             tabBarIcon: ({ color, size }) => (
               <MaterialCommunityIcons
