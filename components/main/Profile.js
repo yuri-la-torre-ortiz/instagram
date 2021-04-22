1d090c055c7a470a7729e018d41219c59946f8f9
Yuri La Torre Ortiz <yuri.la.torre.ortiz@protonmail.ch>
2021-04-22 19:38:09 +0200

    feat: display follow button which allows user to follow another user

--- components/main/Profile.js
+++ components/main/Profile.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, Image, FlatList, StyleSheet } from "react-native";
+import { View, Text, Image, FlatList, StyleSheet, Button } from "react-native";
 
 import firebase from "firebase";
 require("firebase/firestore");
@@ -9,6 +9,7 @@ import { connect } from "react-redux";
 function Profile(props) {
   const [userPosts, setUserPosts] = useState([]);
   const [user, setUser] = useState(null);
+  const [following, setFollowing] = useState(false);
 
   useEffect(() => {
     const { currentUser, posts } = props;
@@ -49,6 +50,26 @@ function Profile(props) {
     }
   }, [props.route.params.uid]);
 
+  const onFollow = () => {
+    firebase
+      .firestore()
+      .collection("following")
+      .doc(firebase.auth().currentUser.uid)
+      .collection("userFollowing")
+      .doc(props.route.params.uid)
+      .set({});
+  };
+
+  const onUnFollow = () => {
+    firebase
+      .firestore()
+      .collection("following")
+      .doc(firebase.auth().currentUser.uid)
+      .collection("userFollowing")
+      .doc(props.route.params.uid)
+      .delete();
+  };
+
   if (user === null) {
     return <View />;
   }
@@ -57,6 +78,16 @@ function Profile(props) {
       <View style={styles.userInfoContainer}>
         <Text>{user.name}</Text>
         <Text>{user.email}</Text>
+
+        {props.route.params.uid !== firebase.auth().currentUser.uid ? (
+          <View>
+            {following ? (
+              <Button title="Following" onPress={() => onUnFollow()} />
+            ) : (
+              <Button title="Follow" onPress={() => onFollow()} />
+            )}
+          </View>
+        ) : null}
       </View>
       <View style={styles.galleryContainer}>
         <FlatList
