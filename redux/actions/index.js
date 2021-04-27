bf89d89c5d7312a404485612d38976aaa50535e0
Yuri La Torre Ortiz <yuri.la.torre.ortiz@protonmail.ch>
2021-04-27 23:01:02 +0200

    feat: retrieve users followed data and posts

--- redux/actions/index.js
+++ redux/actions/index.js
@@ -2,6 +2,8 @@ import {
   USER_STATE_CHANGE,
   USER_POSTS_STATE_CHANGE,
   USER_FOLLOWING_STATE_CHANGE,
+  USERS_DATA_STATE_CHANGE,
+  USERS_POSTS_STATE_CHANGE,
 } from "../constants/index";
 import firebase from "firebase";
 require("firebase/firestore");
@@ -56,6 +58,63 @@ export function fetchUserFollowing() {
           return id;
         });
         dispatch({ type: USER_FOLLOWING_STATE_CHANGE, following });
+        for (let i = 0; i < following.length; i++) {
+          dispatch(fetchUsersData(following[i]));
+        }
+      });
+  };
+}
+
+export function fetchUsersData(uid) {
+  return (dispatch, getState) => {
+    const found = getState().usersState.users.some((el) => el.uid === uid);
+    if (!found) {
+      firebase
+        .firestore()
+        .collection("users")
+        .doc(uid)
+        .get()
+        .then((snapshot) => {
+          if (snapshot.exists) {
+            let user = snapshot.data();
+            user.uid = snapshot.id;
+
+            dispatch({
+              type: USERS_DATA_STATE_CHANGE,
+              user,
+            });
+            dispatch(fetchUsersFollowingPosts(uid));
+          } else {
+            console.log("does not exist.");
+          }
+        });
+    }
+  };
+}
+
+export function fetchUsersFollowingPosts(uid) {
+  return (dispatch, getState) => {
+    firebase
+      .firestore()
+      .collection("posts")
+      .doc(uid)
+      .collection("userPosts")
+      .orderBy("creation", "asc")
+      .get()
+      .then((snapshot) => {
+        console.log(typeof(snapshot.query));
+        const uid = snapshot.query.EP.path.segments[1];
+        console.log({ snapshot, uid });
+        const user = getState().usersState.users.find((el) => el.uid === uid);
+
+        let posts = snapshot.docs.map((doc) => {
+          const data = doc.data();
+          const id = doc.id;
+          return { id, ...data, user };
+        });
+        console.log(posts);
+        dispatch({ type: USERS_POSTS_STATE_CHANGE, posts, uid });
+        console.log(getState());
       });
   };
 }
